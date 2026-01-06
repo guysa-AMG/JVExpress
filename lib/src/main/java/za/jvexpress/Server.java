7c4a1842edd42d03b8487fd77fe4b1c5aa9232e6
gumumjhb025 <moosathejoker@gmail.com>
2026-01-06 19:55:36 +0200

    request parser added

--- lib/src/main/java/za/jvexpress/Server.java
+++ lib/src/main/java/za/jvexpress/Server.java
@@ -1,14 +1,19 @@
 package za.jvexpress;
+
 import java.net.*;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
+
 import za.jvexpress.utils.RequestContext;
 import za.jvexpress.utils.Log;
-
+import za.jvexpress.utils.Log.LogLevel;
+import za.jvexpress.struct.Request;
+import java.lang.Runtime;
 
 public  class Server{
     private ServerSocketChannel sock; 
@@ -35,23 +40,67 @@ public  class Server{
         log.eprint(e);
     }
       log = new Log();
+      Runtime.getRuntime().addShutdownHook(new Thread(()->{this.onClose();}));
     }
-
+  
+    public void onClose(){
+         try{
+                log.print("Closing Application");
+                selector.close();
+                sock.close();
+            }catch(IOException ex){
+                log.eprint(ex);
+            }
+        
+    }
     public  void init(){
        InetSocketAddress bindpoint =new InetSocketAddress("0.0.0.0",8080);
         try {
             sock.bind(bindpoint);
             sock.configureBlocking(false);
             sock.register(selector, SelectionKey.OP_ACCEPT);
-            log.print("Successfully binded to {}".formatted(port));
+            log.print("Successfully binded to %S".formatted(this.port));
         }
          catch (IOException e) {
         log.eprint(e);
         }  
     }
+    public void RequestHandler(SelectionKey key,RequestContext context){
+        String res =context.getData();
+       
+        
+        Request req = Request.fromData(res);
+         log.print(req.getMethod());
+        log.route_print(req.getPath());
+    }
     public void RequestInterpreter(SelectionKey key){
 
     SocketChannel client = (SocketChannel) key.channel();
+    RequestContext currentContext = (RequestContext) key.attachment();
+
+    if (currentContext == null){
+        currentContext = new RequestContext();
+        key.attach(currentContext);
+    }
+    try{
+        int read = client.read(currentContext.byteBuffer);
+        if (read==-1){
+            client.close();
+            return ;
+        }
+        if (read>0){
+            currentContext.append_data(read);
+        }
+        if (currentContext.isComplete){
+            key.interestOps(SelectionKey.OP_WRITE);
+           RequestHandler(key,currentContext);
+        }
+
+    }catch(IOException ex){
+        log.eprint(ex);
+    }
+
+
     
     }
     public void Acceptence(SelectionKey select){
@@ -67,8 +116,9 @@ public  class Server{
              }
     }
     public void listen(){
-        boolean loop=true;
-
+        boolean loop=true;                                         
+        log.libprint("[initializer] Web Server Started",LogLevel.VERBOSE);
+        
         while (loop) { 
             try {
             selector.select();
@@ -91,9 +141,10 @@ public  class Server{
             
 
         }
+       
     }
     
 
 
-
 }
+
